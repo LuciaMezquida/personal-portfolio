1d8181690374e2aa5f1f4d11374a37199483bb02
Lucia Mezquida <lucia.mezquida@gmail.com>
2020-11-29 17:39:47 +0100

    Use node and gulp to create the project

--- js/script.js
+++
@@ -1,8 +0,0 @@
-const change = () => {
-    document.getElementById("email").innerHTML = "lucia.mezquida@gmail.com";
-}
-
-const noChange = () => {
-    document.getElementById("email").innerHTML = "Email";
-}
-    
---
+++ src/js/main.js
@@ -0,0 +1,9 @@
+"use strict";
+
+const change = () => {
+  document.getElementById("email").innerHTML = "lucia.mezquida@gmail.com";
+};
+
+const noChange = () => {
+  document.getElementById("email").innerHTML = "Email";
+};
