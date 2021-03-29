efaaa54ddaaafe53c51cae3366ecf5b6a16998be
Lucia Mezquida <lucia.mezquida@gmail.com>
2021-03-29 13:22:54 +0200

    Add animation to job messages

--- src/js/main.js
+++ src/js/main.js
@@ -1,21 +1,5 @@
 "use strict";
 
-const messages = [
-  "Soy creativa",
-  "Me gusta aprender cosas nuevas",
-  "Soy Frontend Developer Jr.",
-];
-let index = 0;
-const changeHeaderMessage = () => {
-  const headerMessage = document.querySelector(".js-welcome-job");
-  if (index >= 3) {
-    index = 0;
-  }
-  headerMessage.innerHTML = messages[index];
-  index++;
-};
-setInterval(changeHeaderMessage, 2000);
-
 //Bubbles entrance
 const bubbles = document.querySelectorAll(".about__column2--bubble");
 window.addEventListener("scroll", () => {
